37c7475c58c2a095cb72f95f1da147be361c8abe
lior gishry <liorgish@gmail.com>
2019-06-22 17:39:11 +0300

    my solution

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,110 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
-    MaterialApp(
+  return runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.purple,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+//          backgroundColor: Colors.red,
         ),
         body: DicePage(),
       ),
-    ),
-  );
+    );
+  }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var num_1 = 1;
+  var num_2 = 1;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    num_1 = Random().nextInt(5) + 1;
+    num_2 = Random().nextInt(5) + 1;
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              color: Colors.red.shade400,
+              onPressed: () {
+                setState(() {
+                  num_1 = Random().nextInt(5) + 1;
+                  num_2 = Random().nextInt(5) + 1;
+                });
+              },
+              child: Text('Dicee'),
+            ),
+          ],
+        ),
+        Container(
+          color: Colors.yellowAccent,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: RaisedButton(
+                  child: Padding(
+                    padding: const EdgeInsets.all(38.0),
+                    child: Image(
+                      image:
+                          AssetImage('images/dice' + num_1.toString() + '.png'),
+//              width: 100,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      num_1 = Random().nextInt(5) + 1;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: RaisedButton(
+                  child: Padding(
+                    padding: const EdgeInsets.all(38.0),
+                    child: Image(
+                      image:
+                          AssetImage('images/dice' + num_2.toString() + '.png'),
+//              width: 100,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      num_2 = Random().nextInt(5) + 1;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
